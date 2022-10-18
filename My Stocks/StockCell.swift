7d63f682a47617b1090c3a98dc6c55f5128ecb34
DmitryHSE <dmitrykuplinov2017@gmail.com>
2022-10-18 16:47:20 +0500

    Designed the castom cell

---
+++ My Stocks/StockCell.swift
@@ -0,0 +1,19 @@
+//
+//  StockCell.swift
+//  My Stocks
+//
+//  Created by Dmitry on 18.10.2022.
+//
+
+import UIKit
+
+class StockCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+    
+}
